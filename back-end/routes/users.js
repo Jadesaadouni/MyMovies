0d5fc787373f328971f5297dcc56aea255498b79
Jadesaadouni <114593628+Jadesaadouni@users.noreply.github.com>
2023-01-18 23:10:29 +0100

    create routes in users and create token

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -1,4 +1,11 @@
-const express = require ("express");
-const router = express.Router();
+const express = require("express")
+const userControllers = require("../controllers/users")
+const router = express.Router()
+const checkToken = require("../controllers/checkToken");
 
-const usersControllers = require ()
+
+router.post("/users/", checkToken.checkToken, userControllers.postUsers)
+router.get("/users/:id", checkToken.checkToken, userControllers.getUserByUserId)
+router.post("/login", userControllers.login)
+
+module.exports = router
