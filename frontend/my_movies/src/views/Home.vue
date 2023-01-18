0a8926ceb91f604bd3eb876ef66174728dbbbf8e
Jadesaadouni <114593628+Jadesaadouni@users.noreply.github.com>
2023-01-18 23:46:43 +0100

    create new file "homepage" with axios

---
+++ frontend/my_movies/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  components:{
+    MoviesComponent
+  },
+  data(){
+    return{movie_list=[]}
+  },
+async mounted(){
+  try {
+    const response = await axios.get()'https://github.com/Jadesaadouni/MyMovies/',
+        this.movie_list = response.data()
+  }catch (error){
+    console.error(error);
+     }
+  }}
+</script>
+
+<style scoped>
+
+</style>
